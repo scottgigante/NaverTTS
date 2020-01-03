2c3c463688fe42d4c714f95b5c23cfba9ecbf1e4
Scott Gigante <scottgigante@users.noreply.github.com>
2020-01-03 17:52:31 -0500

    Remove 2.7 from classifiers

--- setup.py
+++ setup.py
@@ -50,7 +50,6 @@ setup(
 	'Operating System :: POSIX',
 	'Operating System :: POSIX :: Linux',
 	'Operating System :: Microsoft :: Windows',
-	'Programming Language :: Python :: 2.7',
 	'Programming Language :: Python :: 3.5',
 	'Programming Language :: Python :: 3.6',
 	'Programming Language :: Python :: 3.7',
