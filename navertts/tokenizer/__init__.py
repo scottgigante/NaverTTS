982533478995d33acbf6ce6e93169a43c0946a82
Scott Gigante <scottgigante@gmail.com>
2021-02-15 12:34:53 -0500

    fix import

--- navertts/tokenizer/__init__.py
+++ navertts/tokenizer/__init__.py
@@ -4,9 +4,14 @@ from .core import PreProcessorSub
 from .core import RegexBuilder
 from .core import Tokenizer
 
+from . import pre_processors
+from . import tokenizer_cases
+
 __all__ = [
     "RegexBuilder",
     "PreProcessorRegex",
     "PreProcessorSub",
     "Tokenizer",
+    "pre_processors",
+    "tokenizer_cases",
 ]
